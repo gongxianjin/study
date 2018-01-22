ff40b9cbb70d6213c985a5c620f11c16e31e29e6
1070794219 <1070794219@qq.com>
2018-01-22 21:12:59 +0800

    commit

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,9 +6,9 @@ return array(
 
     'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  'localhost', // 服务器地址
-    'DB_NAME'               =>  'ershiyitianxuexi',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
+    'DB_NAME'               =>  'study',          // 数据库名
+    'DB_USER'               =>  'study',      // 用户名
+    'DB_PWD'                =>  '123456',          // 密码
 //    'DB_PWD'                =>  'abcd!234',          // 密码
     'DB_PORT'               =>  '',        // 端口
 
